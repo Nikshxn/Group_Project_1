2b8eaf11bda53b2a7e61635c7b3fdd376b85ef36
JD2929 <jandunk@creonova.com>
2023-08-23 12:43:45 -0300

    Added const for each of the classes in the HTML. Excluding ex-links as it is unnecessary at this time.

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+const asideButton = document.querySelector(aside-button);
+const navButton = document.querySelector(nav-button);
+const dailySurvey = document.querySelector(daily-survery);
+const sectionBlock = document.querySelector(section-block);
+
+
+
